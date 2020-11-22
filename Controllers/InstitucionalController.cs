fdc2b151d106849c4287f3bd44dcc8cf4012e52d
Luciana <luliialonso@hotmail.com>
2020-11-21 21:49:04 -0300

    Agego staff medico desde la bd

--- Controllers/InstitucionalController.cs
+++ Controllers/InstitucionalController.cs
@@ -26,6 +26,25 @@ namespace Proyecto.Controllers
         }
 
          public IActionResult Staff() {
+            ViewBag.CardiologiaClinica = db.Medico.Where(med => med.Especialidad == "Cardiología Clínica");
+            ViewBag.CardiologiaIntervencionista = db.Medico.Where(med => med.Especialidad == "Cardiología Intervencionista"); 
+            ViewBag.CirugiaCardiovascular = db.Medico.Where(med => med.Especialidad == "Cirugía Cardiovascular"); 
+            ViewBag.CardiologiaPediatrica = db.Medico.Where(med => med.Especialidad == "Cardiología Pediátrica"); 
+            ViewBag.Gastroenterologia = db.Medico.Where(med => med.Especialidad == "Gastroenterología"); 
+            ViewBag.CirugiaToracica = db.Medico.Where(med => med.Especialidad == "Cirugía Torácica"); 
+
+            ViewBag.CirugiaColumna = db.Medico.Where(med => med.Especialidad == "Cirugía de Columna"); 
+            ViewBag.CirugiaGeneral = db.Medico.Where(med => med.Especialidad == "Cirugía General");
+            ViewBag.CirugiaPlastica = db.Medico.Where(med => med.Especialidad == "Cirugía Plástica y Reparadora"); 
+            ViewBag.Ginecologia = db.Medico.Where(med => med.Especialidad == "Ginecología"); 
+            ViewBag.Hematologia = db.Medico.Where(med => med.Especialidad == "Hematología"); 
+            ViewBag.Nefrologa = db.Medico.Where(med => med.Especialidad == "Nefrología"); 
+            
+            ViewBag.Neumonologia = db.Medico.Where(med => med.Especialidad == "Neumonología"); 
+            ViewBag.Oncologia = db.Medico.Where(med => med.Especialidad == "Oncología"); 
+            ViewBag.Traumatologia = db.Medico.Where(med => med.Especialidad == "Ortopedia y Traumatología"); 
+            ViewBag.Otorrinolaringologia = db.Medico.Where(med => med.Especialidad == "Otorrinolaringología"); 
+            ViewBag.Urologia = db.Medico.Where(med => med.Especialidad == "Urología"); 
             return View();
         }
 
