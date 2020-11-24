833cde95bf997ccc21e93764f48d8fb097d602d6
Luciana <luliialonso@hotmail.com>
2020-11-24 19:27:44 -0300

    Completo página de prestaciones

---
+++ Views/Institucional/AgregoMedicos.sql
@@ -0,0 +1,52 @@
+-- SQLite
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (58, 'Dr. Pablo Raffaele', 'Nefrología', 'Jefe de Servicio');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (59, 'Dra. Marcela Fortunato', 'Nefrología', 'Médica de Planta');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (60, 'Dra. Alina Martín', 'Nefrología', 'Médica de Planta');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (61, 'Dr. Rodolfo Marrugat', 'Nefrología', 'Médico de Planta');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (62, 'Dr. Gabriel Rabin', 'Nefrología', 'Médico de Planta');
+
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (63, 'Dr. Jorge O. Cáneva', 'Neumonología', 'Jefe de Servicio');
+
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (64, 'Dra. Lorena Alvarenga', 'Oncología', 'Jefa de Servicio');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (65, 'Dr. Guillermo Méndez', 'Oncología', 'Médico de Planta');
+
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (66, 'Dr. Horacio Rivarola Etcheto', 'Ortopedia y Traumatología', 'Jefe de Servicio');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (67, 'Dr. Cesar Perez', 'Ortopedia y Traumatología', 'Médico de Planta');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (68, 'Dr. Nicolas Basilio', 'Ortopedia y Traumatología', 'Médico de Planta');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (69, 'Dr. Diego Rodriguez Hoya', 'Ortopedia y Traumatología', 'Médico de Planta');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (70, 'Dr. Mauricio Gallucci', 'Ortopedia y Traumatología', 'Médico de Planta');
+
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (71, 'Dr. Juan Ramón Curi', 'Otorrinolaringología', 'Jefe de Servicio');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (72, 'Dr. Miguel Moscovicz', 'Otorrinolaringología', 'Médico de Planta');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (73, 'Dr. Domingo José Rivero', 'Otorrinolaringología', 'Médico de Planta');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (74, 'Dra. Silvana Saldaña', 'Otorrinolaringología', 'Médica de Planta');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (75, 'Dra. Julieta Portiglia', 'Otorrinolaringología', 'Médica de Planta');
+
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (76, 'Dr. Pablo Raffaele', 'Urología', 'Jefe de Servicio');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (77, 'Dra. R. Marcela Fortunato', 'Urología', 'Sub-Jefa de Servicio');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (78, 'Dra. Alina Martín', 'Urología', 'Médica de Planta');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (79, 'Dr. Rodolfo Marrugat', 'Urología', 'Médico de Planta');
+INSERT INTO `Medico` (ID, NombreYApellido, Especialidad, RolEnEspecialidad)
+VALUES (80, 'Dr. Gabriel Rabin', 'Urología', 'Médico de Planta');
---
+++ Views/Institucional/AlgunosUpDates.sql
@@ -0,0 +1,31 @@
+-- SQLite
+update Medico set especialidad = 'Cardiología Intervencionista'
+where id=18;
+update Medico set especialidad = 'Cardiología Intervencionista'
+where id=21;
+
+update Medico set especialidad = 'Cardiología Pediátrica'
+where id=32;
+update Medico set especialidad = 'Cardiología Pediátrica'
+where id=33;
+
+update Medico set especialidad = 'Gastroenterología'
+where id=36;
+update Medico set especialidad = 'Ginecología'
+where id=52;
+
+
+update Medico set rolEnEspecialidad = 'Médica de Planta'
+where id=18;
+update Medico set rolEnEspecialidad = 'Médica de Planta'
+where id=21;
+
+update Medico set rolEnEspecialidad = 'Médica de Planta'
+where id=32;
+update Medico set rolEnEspecialidad = 'Médica de Planta'
+where id=33;
+
+update Medico set rolEnEspecialidad = 'Médica de Planta'
+where id=36;
+update Medico set rolEnEspecialidad = 'Médica de Planta'
+where id=52;
